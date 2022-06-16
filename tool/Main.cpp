640e9da6de2cdb08faea7fe54621b2fe0528f380
duri12 <eyalgor102@gmail.com>
2022-06-16 18:59:01 +0300

    fixed the fairy/wizard and the execption print

---
+++ tool/Main.cpp
@@ -0,0 +1,10 @@
+#include "Mtmchkin.h"
+
+int main(){
+    const int MAX_NUMBER_OF_ROUNDS = 100;
+    Mtmchkin game("deck.txt");
+    while(!game.isGameOver()&&game.getNumberOfRounds()<MAX_NUMBER_OF_ROUNDS){
+        game.playRound();
+    }
+    game.printLeaderBoard();
+}
